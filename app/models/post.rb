9538433ae370425e89a78de7a4077c5ae9825d60
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2018-12-01 15:04:47 +0900

    add model association and basic validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+  has_many :images, class_name: 'PostImage'
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :thumbnail, presence: true
+  validates :ctype, presence: true
+  validates :images, length: { maximum: 4 }
 end
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,2 +1,5 @@
 class PostImage < ApplicationRecord
+  belongs_to :post
+  validates :image, presence: true
+  validates :ctype, presence: true
 end
